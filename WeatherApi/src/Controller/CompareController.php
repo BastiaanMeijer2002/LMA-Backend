13a1227c37771732e7e6dd759113603cdf9b5377
bastiaan <b.meijer@st.hanze.nl>
2022-06-09 12:07:57 +0200

    added entrypoint for comparing stations on specific date

--- WeatherApi/src/Controller/CompareController.php
+++ WeatherApi/src/Controller/CompareController.php
@@ -7,6 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\WeatherData;
+use Symfony\Component\Validator\Constraints\DateTime;
 
 #[Route('/compare', name: 'app_compare')]
 class CompareController extends AbstractController
@@ -15,8 +16,8 @@ class CompareController extends AbstractController
 
     public function __construct(private ManagerRegistry $doctrine) {$this->repository = $doctrine->getRepository(WeatherData::class);}
 
-    #[Route('/station/{station1}/{station2}', name: 'app_compare_station',methods: "GET")]
-    public function index(int $station1, int $station2): Response
+    #[Route('/station/{station1}/{station2}/', name: 'app_compare_station',methods: "GET")]
+    public function station(int $station1, int $station2,): Response
     {
         $station1 = $this->repository->findBy(
             ['geolocation'=>$station1],
@@ -25,6 +26,38 @@ class CompareController extends AbstractController
 
         $station2 = $this->repository->findBy(
             ['geolocation'=>$station2],
+            ['date'=>'DESC']
+        );
+
+        $data = [
+            'station1'=>$station1,
+            'station2'=>$station2
+        ];
+
+        return $this->json($data);
+    }
+
+    /**
+     * @throws \Exception
+     */
+    #[Route('/station/{station1}/{station2}/{date}', name: 'app_compare_station_date',methods: "GET")]
+    public function stationDate(int $station1, int $station2, $date): Response
+    {
+        $date = new \DateTime($date);
+
+        $station1 = $this->repository->findBy(
+            [
+                'geolocation'=>$station1,
+                'date'=>$date
+            ],
+            ['date'=>'DESC']
+        );
+
+        $station2 = $this->repository->findBy(
+            [
+                'geolocation'=>$station2,
+                'date'=>$date
+            ],
             ['date'=>'DESC']
         );
 
