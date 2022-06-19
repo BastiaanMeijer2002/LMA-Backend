f7ca348e7ff4cb5b040f850c356f6b3c9dbd26d2
bastiaan <b.meijer@st.hanze.nl>
2022-06-19 14:17:57 +0200

    added jwt login

--- scripts/login_check.sh
+++ scripts/login_check.sh
@@ -1,1 +1,1 @@
- curl -X POST -H "Content-Type: application/json" http://localhost:8000/api/login_check -d '{"email":"bas@bas","password":"1234"}'
+ curl -X POST -H "Content-Type: application/json" http://localhost:8000/api/login_check -d '{"email":"bas@bas","password":"1234", "username":""}'
